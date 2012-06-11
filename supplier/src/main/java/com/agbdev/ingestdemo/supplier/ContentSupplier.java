275fc4ee0af687d1c1fa6591071079df13edc62a
Andrew Bernard <agbernard@gmail.com>
2012-06-10 23:48:34 -0400

    Added more dummy data

--- supplier/src/main/java/com/agbdev/ingestdemo/supplier/ContentSupplier.java
+++ supplier/src/main/java/com/agbdev/ingestdemo/supplier/ContentSupplier.java
@@ -1,5 +1,7 @@
 package com.agbdev.ingestdemo.supplier;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,6 +11,20 @@ import com.agbdev.ingestdemo.content.Movie;
 
 @Path("/content")
 public class ContentSupplier {
+	private static Map<Long, Movie> movies = new HashMap<Long, Movie>();
+	static {
+		long id = 0;
+		initMovie(++id, "Gladiator");
+		initMovie(++id, "The Matrix");
+		initMovie(++id, "The Dark Knight");
+		initMovie(++id, "Equilibrium");
+		initMovie(++id, "Braveheart");
+		initMovie(++id, "Troy");
+		initMovie(++id, "The Shawshank Redemption");
+		initMovie(++id, "300");
+		initMovie(++id, "Ironman");
+		initMovie(++id, "The Avengers");
+	}
 
 	/*
 	 * Test with:
@@ -18,9 +34,18 @@ public class ContentSupplier {
 	@Path("/movies/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Movie getMovie(final @PathParam("id") Long contentId) {
-		//TODO: get movie based on id
+		Movie movie;
+		if(movies.containsKey(contentId)) {
+			movie = movies.get(contentId);
+		}
+		else {
+			movie = movies.get(1L);
+		}
+		return movie;
+	}
 
-		return new Movie() {
+	private static void initMovie(final Long id, final String name) {
+		Movie movie = new Movie() {
 
 			public void setName(final String name) {
 				// not needed
@@ -31,13 +56,15 @@ public class ContentSupplier {
 			}
 
 			public String getName() {
-				return "Gladiator";
+				return name;
 			}
 
 			public Long getId() {
-				return contentId;
+				return id;
 			}
 		};
+
+		movies.put(id, movie);
 	}
 
 }
